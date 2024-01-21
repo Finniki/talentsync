6e0d9141eb9283524141cd4da62c14e4ab81ecea
Finn <mailtorufina@gmail.com>
2024-01-22 00:56:17 +0100

    add: add the navigation links to the nav bar
    this commit also styles the links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,30 @@
+import NavItems from '../NavItems/NavItems'
 import { ReactComponent as Logo } from "../../assets/images/Logo.svg";
 import styles from './Nav.module.css';
 
 const Nav = () => {
+    const navItems = {
+        Products: "#",
+        Solutions: "#",
+        Resource: "#",
+        Pricing: "#"
+    }
     return (
         <div className={styles.nav}>
             <div className={styles.brand}>
                 <Logo className={styles.logo} />
                 <p className={styles.brandName}>ClearLink<span className={styles.blueDot}>.</span></p>
             </div>
+            {
+                <div className={styles.navItems}>
+                    {Object.entries(navItems).map((navItems, index) => {
+                        const [linkName, link] = navItems;
+                        return <NavItems key={linkName} linkName={linkName} link={link} showArrow={index < 3} />;
+                    })}
+                </div>
+            }
         </div>
     )
-    }
+}
 
 export default Nav;
---
+++ src/components/NavItems/NavItems.js
@@ -0,0 +1,13 @@
+import styles from './NavItems.module.css';
+import { ReactComponent as ChevronDown } from "../../assets/icons/chevron-down.svg";
+
+const NavItems = ({ linkName, link, showArrow }) => {
+    return (
+        <div className={styles.navItem}>
+            <a href={link} target="_blank" className={styles.link}>{linkName}</a>
+            {showArrow && <ChevronDown/>}
+        </div>
+    )
+}
+
+export default NavItems;
