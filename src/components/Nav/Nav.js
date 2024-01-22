d59583504c06eb4712c464202d011e02e32f8821
Finn <mailtorufina@gmail.com>
2024-01-22 15:06:24 +0100

    add Sign up buttons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,30 +1,64 @@
-import NavItems from '../NavItems/NavItems'
+import NavItems from "../NavItems/NavItems";
 import { ReactComponent as Logo } from "../../assets/images/Logo.svg";
-import styles from './Nav.module.css';
+import { Button } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../../assets/theme/theme";
+import primary from "../../assets/buttonStyles/primary";
+import secondary from "../../assets/buttonStyles/secondary";
+import styles from "./Nav.module.css";
 
 const Nav = () => {
-    const navItems = {
-        Products: "#",
-        Solutions: "#",
-        Resource: "#",
-        Pricing: "#"
-    }
-    return (
-        <div className={styles.nav}>
-            <div className={styles.brand}>
-                <Logo className={styles.logo} />
-                <p className={styles.brandName}>ClearLink<span className={styles.blueDot}>.</span></p>
-            </div>
-            {
-                <div className={styles.navItems}>
-                    {Object.entries(navItems).map((navItems, index) => {
-                        const [linkName, link] = navItems;
-                        return <NavItems key={linkName} linkName={linkName} link={link} showArrow={index < 3} />;
-                    })}
-                </div>
-            }
+  const navItems = {
+    Products: "#",
+    Solutions: "#",
+    Resource: "#",
+    Pricing: "#",
+  };
+  return (
+    <div className={styles.nav}>
+      <div className={styles.brand}>
+        <Logo className={styles.logo} />
+        <p className={styles.brandName}>
+          ClearLink<span className={styles.blueDot}>.</span>
+        </p>
+      </div>
+      {
+        <div className={styles.navItems}>
+          {Object.entries(navItems).map((navItems, index) => {
+            const [linkName, link] = navItems;
+            return (
+              <NavItems
+                key={linkName}
+                linkName={linkName}
+                link={link}
+                showArrow={index < 3}
+              />
+            );
+          })}
         </div>
-    )
-}
+      }
+      <div className={styles.buttons}>
+        <ThemeProvider theme={theme}>
+          <Button
+            variant="contained"
+            disableElevation
+            color="secondary"
+            sx={secondary}
+          >
+            Talk to Sales
+          </Button>
+          <Button
+            variant="contained"
+            disableElevation
+            color="primary"
+            sx={primary}
+          >
+            Sign up for free
+          </Button>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
