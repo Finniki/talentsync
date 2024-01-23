f340ea80ce21d1da4efdc54ecbd5b94d18499914
Finn <mailtorufina@gmail.com>
2024-01-23 19:07:27 +0100

    add: This commit renders all sections on the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import Nav from "./components/Nav/Nav";
 import HeroSection from "./components/HeroSection/HeroSection";
+import SocialProof from "./components/SocialProof/SocialProof";
+import Features from "./components/Features/Features";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Support from "./components/Support/Support";
+import Trial from "./components/Trial/Trial";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -8,6 +13,12 @@ function App() {
       <header className="header">
         <HeroSection />
       </header>
+      <SocialProof />
+      <Features />
+      <Testimonials />
+      <Support />
+      <Trial />
+      <Footer />
     </div>
   );
 }
